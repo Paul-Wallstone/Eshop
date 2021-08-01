bdbf98fda5401fc8649af0da3e7dca4f0042971d
Pavel Puchinski <pavel.puchinsky@gmail.com>
2021-08-01 16:25:43 +0300

    base functionality

---
+++ src/main/java/by/teachmeskills/eshop/service/impl/HomePageServiceImpl.java
@@ -0,0 +1,17 @@
+package by.teachmeskills.eshop.service.impl;
+
+import by.teachmeskills.eshop.exceptions.ServiceException;
+import by.teachmeskills.eshop.repository.domain.User;
+import by.teachmeskills.eshop.service.interfacefolder.HomePageService;
+
+public class HomePageServiceImpl implements HomePageService {
+
+
+    @Override
+    public User switchUserNameAccount(User user) throws ServiceException {
+        if (user.getName() == null) {
+            user.setName("log in");
+        }
+        return user;
+    }
+}
---
+++ src/main/java/by/teachmeskills/eshop/service/impl/RegistrationPageServiceImpl.java
@@ -0,0 +1,37 @@
+package by.teachmeskills.eshop.service.impl;
+
+import by.teachmeskills.eshop.exceptions.ServiceException;
+import by.teachmeskills.eshop.repository.domain.User;
+import by.teachmeskills.eshop.service.interfacefolder.RegistrationPageService;
+import by.teachmeskills.eshop.service.interfacefolder.UserService;
+import by.teachmeskills.eshop.utils.Validation;
+import by.teachmeskills.eshop.utils.ValidationImpl;
+import org.apache.log4j.Logger;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+
+public class RegistrationPageServiceImpl implements RegistrationPageService {
+    private static final Logger log = Logger.getLogger(RegistrationPageServiceImpl.class);
+
+
+    @Override
+    public Boolean isRegistrateNewUser(User user, String birthday, UserService userService) throws ServiceException {
+        if (Optional.ofNullable(user).isPresent()) {
+            LocalDate localDate = LocalDate.parse(birthday);
+            user.setDateOfBirthday(localDate);
+            Validation validation = new ValidationImpl();
+            if (validation.validateData(user)) {
+                try {
+                    userService.create(user);
+                    return true;
+                } catch (Exception e) {
+                    log.error(e.getMessage());
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
+}
